03288addd0c94af3d3b48aa42b1c646d21e3b430
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2025-11-17 10:30:37 +0000

    GeneradorDominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let end = ['.com', '.net', '.us', '.io', '.es'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function obtenerNumeroAleatorio(max){
+    return Math.floor(Math.random() *max);
+  }
+  function obtenerElemento(){
+    let dominio = `${pronoun[obtenerNumeroAleatorio (pronoun.length)]}` + `${adj[obtenerNumeroAleatorio (adj.length)]}` + `${noun[obtenerNumeroAleatorio (noun.length)]}` + `${end[obtenerNumeroAleatorio (end.length)]}`
+  document.getElementById("dominio").innerHTML = dominio;
+  }
+  obtenerElemento()
+}
